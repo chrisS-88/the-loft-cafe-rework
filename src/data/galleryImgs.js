f67c3f7ba02b44ba108f7671ca36dc2caadaa60a
Chris Smart <chrissmart920@gmail.com>
2026-02-19 18:26:14 +0000

    added gallery page

---
+++ src/data/galleryImgs.js
@@ -0,0 +1,63 @@
+export const galleryImages = [
+  {
+    src: "../src/assets/images/gallery_images/gallery_1.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_2.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_3.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_4.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_5.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_6.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_7.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_8.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_9.jpg",
+    alt: "...",
+  },
+
+  {
+    src: "../src/assets/images/gallery_images/gallery_11.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_12.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_13.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_14.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_15.jpg",
+    alt: "...",
+  },
+  {
+    src: "../src/assets/images/gallery_images/gallery_16.jpg",
+    alt: "...",
+  },
+];
