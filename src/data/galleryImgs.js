47522d1b1f1df8ee2d76f2acc4a1148760c225a5
Chris Smart <chrissmart920@gmail.com>
2026-02-20 07:49:53 +0000

    gallery page changed img paths

--- src/data/galleryImgs.js
+++ src/data/galleryImgs.js
@@ -1,63 +1,83 @@
+import g1 from "../assets/images/gallery_images/gallery_1.jpg";
+import g2 from "../assets/images/gallery_images/gallery_2.jpg";
+import g3 from "../assets/images/gallery_images/gallery_3.jpg";
+import g4 from "../assets/images/gallery_images/gallery_4.jpg";
+import g5 from "../assets/images/gallery_images/gallery_5.jpg";
+import g6 from "../assets/images/gallery_images/gallery_6.jpg";
+import g7 from "../assets/images/gallery_images/gallery_7.jpg";
+import g8 from "../assets/images/gallery_images/gallery_8.jpg";
+import g9 from "../assets/images/gallery_images/gallery_9.jpg";
+import g10 from "../assets/images/gallery_images/gallery_10.jpg";
+import g11 from "../assets/images/gallery_images/gallery_11.jpg";
+import g12 from "../assets/images/gallery_images/gallery_12.jpg";
+import g13 from "../assets/images/gallery_images/gallery_13.jpg";
+import g14 from "../assets/images/gallery_images/gallery_14.jpg";
+import g15 from "../assets/images/gallery_images/gallery_15.jpg";
+import g16 from "../assets/images/gallery_images/gallery_16.jpg";
+
 export const galleryImages = [
   {
-    src: "../src/assets/images/gallery_images/gallery_1.jpg",
-    alt: "...",
+    src: g1,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_2.jpg",
-    alt: "...",
+    src: g2,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_3.jpg",
-    alt: "...",
+    src: g3,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_4.jpg",
-    alt: "...",
+    src: g4,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_5.jpg",
-    alt: "...",
+    src: g5,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_6.jpg",
-    alt: "...",
+    src: g6,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_7.jpg",
-    alt: "...",
+    src: g7,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_8.jpg",
-    alt: "...",
+    src: g8,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_9.jpg",
-    alt: "...",
-  },
-
-  {
-    src: "../src/assets/images/gallery_images/gallery_11.jpg",
-    alt: "...",
+    src: g9,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_12.jpg",
-    alt: "...",
+    src: g10,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_13.jpg",
-    alt: "...",
+    src: g11,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_14.jpg",
-    alt: "...",
+    src: g12,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_15.jpg",
-    alt: "...",
+    src: g13,
+    alt: "Gallery Image",
   },
   {
-    src: "../src/assets/images/gallery_images/gallery_16.jpg",
-    alt: "...",
+    src: g14,
+    alt: "Gallery Image",
+  },
+  {
+    src: g15,
+    alt: "Gallery Image",
+  },
+  {
+    src: g16,
+    alt: "Gallery Image",
   },
 ];
